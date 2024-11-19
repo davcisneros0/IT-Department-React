b2cb7cbb8fe290317cbc5dfd2a7de9268d9fd6e1
David <Davcisneros2000@gmail.com>
2024-11-19 10:19:21 -0500

    Add each webpage for navigation menu / routing

--- it-department-site/src/App.js
+++ it-department-site/src/App.js
@@ -1,8 +1,39 @@
 import './styles.css';
+import Home from "./pages/Home";
 import Navbar from "./Navbar";
+import About from "./pages/About";
+import Programs from "./pages/Programs";
+import Faculty from "./pages/Faculty";
+import Contact from "./pages/Contact";
 
 function App() {
-  return <Navbar />
+
+  let component;
+
+  //WEBPAGE ROUTING
+  switch (window.location.pathname) {
+    case "/":
+      component = <Home />;
+      break;
+    case "/about":
+      component = <About />;
+      break;
+    case "/programs":
+      component = <Programs />;
+        break;
+    case "/faculty":
+      component = <Faculty />;
+        break;
+    case "/contact":
+      component = <Contact />;
+        break;
+  }
+  return (
+  <>
+    <Navbar />
+    {component}
+  </>
+  );
 }
 
 export default App;
