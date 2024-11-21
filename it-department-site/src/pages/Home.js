b3ad5085056302814306a952f6f7a1981601b02e
David <Davcisneros2000@gmail.com>
2024-11-21 10:22:36 -0500

    Add home banner and CSS to Home.js

--- it-department-site/src/pages/Home.js
+++ it-department-site/src/pages/Home.js
@@ -5,8 +5,16 @@ export default function Home() {
         //Contact Info
         //Call to action (Apply Now! Request Info)
     <div className="home-page">
-        <h1>Home</h1>
-        
+        <div id="home-banner">
+    
+            <div id="home-banner-text">
+                <h1>Information Technology</h1>
+            </div>
+        </div>
+
+        <div id="mission-statement">
+            <h2>Mission</h2>
+        </div>
     </div>
     )
 }
