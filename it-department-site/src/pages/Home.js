826b2c0e78c79d59418c0632a2afff8acf43b1b7
David <Davcisneros2000@gmail.com>
2024-11-21 22:57:38 -0500

    Add mission statement container, text, and <hr>

--- it-department-site/src/pages/Home.js
+++ it-department-site/src/pages/Home.js
@@ -4,6 +4,7 @@ export default function Home() {
         //Featured programs/courses
         //Contact Info
         //Call to action (Apply Now! Request Info)
+        // https://www.ncf.edu/departments/information-technnology/
     <div className="home-page">
         <div id="home-banner">
     
@@ -13,7 +14,44 @@ export default function Home() {
         </div>
 
         <div id="mission-statement">
-            <h2>Mission</h2>
+
+            <div id="mission-statement-header">
+                <h2>Mission</h2>
+            </div>
+
+            <div class="section-container">
+                <p>
+                The Information Technology (IT) Department at Broward College strives to provide students with a comprehensive 
+                and forward-thinking education that prepares them for the demands of the rapidly changing technology industry. 
+                Our mission is to cultivate a learning environment that emphasizes hands-on experience, problem-solving, and 
+                critical thinking. 
+                <br>
+                </br>
+                <br>
+                </br>
+                Students are equipped with the skills needed to excel in various IT fields, including 
+                software development, network management, and cybersecurity, ensuring they are ready to face the challenges 
+                of an increasingly digital world. By combining a solid academic foundation with real-world applications, we aim 
+                to produce graduates who are leaders and innovators in technology.
+                </p>
+                <br>
+                </br>
+                <p>
+                The IT Department at Broward College offers a diverse array of programs designed to give students a broad 
+                understanding of the IT landscape while allowing them to specialize in key areas such as cloud technologies, data 
+                management, and information security. Students gain access to cutting-edge labs, software, and resources that 
+                simulate real-world IT environments.
+                <br>
+                </br>
+                <br>
+                </br>
+                With expert faculty members and strong ties to industry, the department 
+                provides students with opportunities for internships, networking, and career development, ensuring they are 
+                well-prepared for successful careers in the ever-evolving field of information technology.
+                </p>
+            </div>
+
+            <hr></hr>
         </div>
     </div>
     )
