a95302f3c059ff7f598272a3541aa8eb638ba02f
David <Davcisneros2000@gmail.com>
2024-11-27 10:14:51 -0500

    Add CSS and Media query for faculty list and faculty list items

--- it-department-site/src/pages/Contact.js
+++ it-department-site/src/pages/Contact.js
@@ -1,3 +1,5 @@
 export default function Contact() {
+    // Contact information, including address, phone number, and email
+    // Contact form 
     return <h1>Contact</h1>
 }
--- it-department-site/src/pages/Faculty.js
+++ it-department-site/src/pages/Faculty.js
@@ -1,3 +1,184 @@
 export default function Faculty() {
-    return <h1>Faculty</h1>
+    return ( 
+        <div className="faculty-page">
+            <div id="faculty-statement">
+
+                <div id="faculty-statement-header">
+                    <h2>Faculty</h2>
+                </div>
+
+                <div class="section-container">
+                    <p>The Broward College directory contains public information on current faculty and staff. 
+                        Every effort has been made to make the contents of this Directory as up-to-date as possible. 
+                        <br></br><br></br>
+                        However, we cannot guarantee or assume responsibility for complete accuracy of all the 
+                        information found in the directory, complete absence of errors or omissions, or presence of 
+                        any unintentional directory entries.</p>
+                </div>
+
+                <hr></hr>
+            </div>
+
+            <div id="faculty-list">
+
+                    <div class="contact-items">
+                        <div class="contact-items-image">
+                            <img src="https://static.vecteezy.com/system/resources/thumbnails/048/334/475/small_2x/a-person-icon-on-a-transparent-background-png.png" 
+                            alt="A person icon on a transparent background"></img>
+                        </div>
+
+                        <div class="contact-items-info">
+                            <p>John Doe</p>
+                            <span>Systems Administrator</span>
+                            <h5>Contact</h5>
+                                <a href="mailto:johndoe@bc.edu">johndoe@bc.edu</a>
+                                <br></br><br></br>
+                                <a href="tel:777-777-7777">777-777-7777</a>
+                        </div>
+                    </div>
+
+                    <div class="contact-items">
+                        <div class="contact-items-image">
+                            <img src="https://static.vecteezy.com/system/resources/thumbnails/048/334/475/small_2x/a-person-icon-on-a-transparent-background-png.png"
+                            alt="A person icon on a transparent background"></img>
+                        </div>
+
+                        <div class="contact-items-info">
+                            <p>Jane Doe</p>
+                            <span>Chief Information Officer</span>
+                            <h5>Contact</h5>
+                                <a href="mailto:janedoe@bc.edu">janedoe@bc.edu</a>
+                                <br></br><br></br>
+                                <a href="tel:777-777-7777">777-777-7777</a>
+                        </div>
+                    </div>
+
+                    <div class="contact-items">
+                        <div class="contact-items-image">
+                            <img src="https://static.vecteezy.com/system/resources/thumbnails/048/334/475/small_2x/a-person-icon-on-a-transparent-background-png.png"
+                            alt="A person icon on a transparent background"></img>
+                        </div>
+
+                        <div class="contact-items-info">
+                            <p>Johnny Doe</p>
+                            <span>Dean of Information Technology</span>
+                            <h5>Contact</h5>
+                                <a href="mailto:johnnydoe@bc.edu">johnnydoe@bc.edu</a>
+                                <br></br><br></br>
+                                <a href="tel:777-777-7777">777-777-7777</a>
+                        </div>
+                    </div>
+
+                    <div class="contact-items">
+                        <div class="contact-items-image">
+                            <img src="https://static.vecteezy.com/system/resources/thumbnails/048/334/475/small_2x/a-person-icon-on-a-transparent-background-png.png"
+                            alt="A person icon on a transparent background"></img>
+                        </div>
+
+                        <div class="contact-items-info">
+                            <p>Jon Doe</p>
+                            <span>Director, Campus Technology</span>
+                            <br></br>
+                            <span>Central Campus</span>
+                            <h5>Contact</h5>
+                                <a href="mailto:jondoe@bc.edu">jondoe@bc.edu</a>
+                                <br></br><br></br>
+                                <a href="tel:777-777-7777">777-777-7777</a>
+                        </div>
+                    </div>
+
+                    <div class="contact-items">
+                        <div class="contact-items-image">
+                            <img src="https://static.vecteezy.com/system/resources/thumbnails/048/334/475/small_2x/a-person-icon-on-a-transparent-background-png.png"
+                            alt="A person icon on a transparent background"></img>
+                        </div>
+
+                        <div class="contact-items-info">
+                            <p>Jake Doe</p>
+                            <span>Director, Campus Technology</span>
+                            <br></br>
+                            <span>South Campus</span>
+                            <h5>Contact</h5>
+                                <a href="mailto:jakedoe@bc.edu">jakedoe@bc.edu</a>
+                                <br></br><br></br>
+                                <a href="tel:777-777-7777">777-777-7777</a>
+                        </div>
+                    </div>
+
+                    <div class="contact-items">
+                        <div class="contact-items-image">
+                            <img src="https://static.vecteezy.com/system/resources/thumbnails/048/334/475/small_2x/a-person-icon-on-a-transparent-background-png.png"
+                            alt="A person icon on a transparent background"></img>
+                        </div>
+
+                        <div class="contact-items-info">
+                            <p>Jonny Doe</p>
+                            <span>Director, Campus Technology</span>
+                            <br></br>
+                            <span>North Campus</span>
+                            <h5>Contact</h5>
+                                <a href="mailto:jonnydoe@bc.edu">jonnydoe@bc.edu</a>
+                                <br></br><br></br>
+                                <a href="tel:777-777-7777">777-777-7777</a>
+                        </div>
+                    </div>
+
+                    <div class="contact-items">
+                        <div class="contact-items-image">
+                            <img src="https://static.vecteezy.com/system/resources/thumbnails/048/334/475/small_2x/a-person-icon-on-a-transparent-background-png.png" 
+                            alt="A person icon on a transparent background"></img>
+                        </div>
+
+                        <div class="contact-items-info">
+                            <p>Jose Doe</p>
+                            <span>Senior Web Developer</span>
+                            <br></br>
+                            <span>Central Campus</span>
+                            <h5>Contact</h5>
+                                <a href="mailto:josedoe@bc.edu">josedoe@bc.edu</a>
+                                <br></br><br></br>
+                                <a href="tel:777-777-7777">777-777-7777</a>
+                        </div>
+                    </div>
+
+                    <div class="contact-items">
+                        <div class="contact-items-image">
+                            <img src="https://static.vecteezy.com/system/resources/thumbnails/048/334/475/small_2x/a-person-icon-on-a-transparent-background-png.png"
+                            alt="A person icon on a transparent background"></img>
+                        </div>
+
+                        <div class="contact-items-info">
+                            <p>Jenny Doe</p>
+                            <span>Senior Web Developer</span>
+                            <br></br>
+                            <span>South Campus</span>
+                            <h5>Contact</h5>
+                                <a href="mailto:jennydoe@bc.edu">jennydoe@bc.edu</a>
+                                <br></br><br></br>
+                                <a href="tel:777-777-7777">777-777-7777</a>
+                        </div>
+                    </div>
+
+                    <div class="contact-items">
+                        <div class="contact-items-image">
+                            <img src="https://static.vecteezy.com/system/resources/thumbnails/048/334/475/small_2x/a-person-icon-on-a-transparent-background-png.png"
+                            alt="A person icon on a transparent background"></img>
+                        </div>
+
+                        <div class="contact-items-info">
+                            <p>Justin Doe</p>
+                            <span>Senior Web Developer</span>
+                            <br></br>
+                            <span>North Campus</span>
+                            <h5>Contact</h5>
+                                <a href="mailto:justindoe@bc.edu">justindoe@bc.edu</a>
+                                <br></br><br></br>
+                                <a href="tel:777-777-7777">777-777-7777</a>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+    )
 }
