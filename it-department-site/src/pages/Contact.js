3340dba48b343094a6f0f8a00dec42afcc45421f
David <Davcisneros2000@gmail.com>
2024-11-27 23:54:57 -0500

    Add HTML elements for form and contact body

--- it-department-site/src/pages/Contact.js
+++ it-department-site/src/pages/Contact.js
@@ -1,5 +1,60 @@
 export default function Contact() {
     // Contact information, including address, phone number, and email
     // Contact form 
-    return <h1>Contact</h1>
+    return (
+        <div className="contact-page">
+            <div id="contact-statement">
+
+                <div id="contact-statement-header">
+                    <h2>Contact</h2>
+                </div>
+
+                <div class="section-container">
+                    <p>Our goal is to prepare and provide professional, courteous, accurate, and timely communication 
+                        services at a superior level. We embrace a shared community of respect to our internal and 
+                        external clients.</p>
+                </div>
+            </div>
+
+            <div className="contact-body">
+                <div class="contact-info">
+                    <div>
+                        <span><i class="fa fa-solid fa-phone"></i></span>
+                        {/* <span>Phone Number</span> */}
+                        <span className="contact-text">954-201-6723</span>
+                    </div>
+
+                    <div>
+                        <span><i class="fa fa-solid fa-envelope"></i></span>
+                        {/* <span>Email</span> */}
+                        <span className="contact-text">computer-science@broward.edu</span>
+                    </div>
+
+                    <div>
+                        <span><i class="fa fa-solid fa-map-pin"></i></span>
+                        {/* <span>Address</span> */}
+                        <span className="contact-text">Central Campus, Building 13, 3501 Davie Road, Davie, FL 33314</span>
+                    </div>
+                </div>
+
+            <div className="contact-form">
+                <form>
+                    <div>
+                        <input type="text" className="form-control" placeholder="First Name"></input>
+                        <input type="text" className="form-control" placeholder="Last Name"></input>
+                    </div>
+                    <div>
+                        <input type="email" className="form-control" placeholder="Email"></input>
+                        <input type="text" className="form-control" placeholder="Phone"></input>
+                    </div>
+                    <div>
+                        <textarea rows="5" placeholder="Message" className="form-control"></textarea>
+                        <input type="submit" className="send-btn" value="Send"></input>
+                    </div>
+                </form>
+            </div>
+
+            </div>
+        </div>
+    )
 }
